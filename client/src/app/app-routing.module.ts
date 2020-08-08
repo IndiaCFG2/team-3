93257501703b954d580864c3520b83e46e3c99b1
Tanisha Tambi <tanishatambi01@gmail.com>
2020-08-08 16:32:48 +0530

    adding lesson component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { pathToFileURL } from 'url';
+import { LessonComponent } from './lesson/lesson.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path:'lessons', component: LessonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LessonComponent } from './lesson/lesson.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LessonComponent,
   ],
   imports: [
     BrowserModule,
