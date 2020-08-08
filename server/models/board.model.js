fade256e85a08014a90ed1e4686c0d00b25a8f5c
Abhishek Sen <abhishek13dgp5@gmail.com>
2020-08-08 14:01:03 +0530

    init models

---
+++ server/models/board.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const boardSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    code: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+const Board = mongoose.model('Board', boardSchema);
+
+module.exports = Board;
