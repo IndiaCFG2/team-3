5352f31dce2aa30bd2bd504d166625f02673980f
Abhishek Sen <abhishek13dgp5@gmail.com>
2020-08-10 12:53:37 +0530

    Removed private credentials

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,14 +14,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit(){
     const firebaseConfig ={
-      apiKey: "AIzaSyAaUPOugUpE54lTHnLBu-r3Tb8o2eYwMiM",
-      authDomain: "imagestorer-9e996.firebaseapp.com",
-      databaseURL: "https://imagestorer-9e996.firebaseio.com",
-      projectId: "imagestorer-9e996",
-      storageBucket: "imagestorer-9e996.appspot.com",
-      messagingSenderId: "292156459378",
-      appId: "1:292156459378:web:cc74d08702dee80854f5eb",
-      measurementId: "G-C8XTPD9WVL"
+      // firebase config goes here
+      // Please provide valid firebase config for the app to run
     }
     firebase.initializeApp(firebaseConfig);
   }
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI; // Please write a valid ATLAS_URI in .env file
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
